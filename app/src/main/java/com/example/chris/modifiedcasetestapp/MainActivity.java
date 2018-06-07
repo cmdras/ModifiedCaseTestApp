fd82f8b6200363f117cae6df2607d773862c498f
chris <cmdras94@gmail.com>
2018-06-07 14:49:38 +0200

    second commit

--- app/src/main/java/com/example/chris/modifiedcasetestapp/MainActivity.java
+++ app/src/main/java/com/example/chris/modifiedcasetestapp/MainActivity.java
@@ -12,15 +12,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static void helloworld () throws IOException {
+        System.out.println("Hello, World");
+    }
+
     public static void foo1 () throws IOException {
         FileOutputStream fos1 = new FileOutputStream(new File("whatever.txt"));
         fos1.write(10);   //DOH! What if exception?
         fos1.close();
-    }
-
-    public static void foo2 () throws IOException {
-        FileOutputStream fos2 = new FileOutputStream(new File("whatever.txt"));
-        fos2.write(10);   //DOH! What if exception?
-        fos2.close();
     }
 }
